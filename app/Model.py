eac28ca763714675e7251c7d393037cb5e7f8cbd
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-12-06 10:41:13 -0500

    Added model and shcmea for the user

--- app/Model.py
+++ app/Model.py
@@ -5,3 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 ma = Marshmallow()
 db = SQLAlchemy()
+
+class User(db.Model):
+    """ Model for the user
+    :type id: int
+    :type username: str
+    :type password: str
+    :type job: str
+    :type notebooks: [Notebook]
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    job = db.Column(db.String(250), nullable=True)
+    notebooks = db.relationship('Notebook', backref='user', lazy=True)
+
+class UserSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    username = fields.String(required=True, validate=validate.Length(3))
+    password = fields.String(required=True, validate=validate.Length(8))
+    job = fields.String(validate=validate.Length(2))
